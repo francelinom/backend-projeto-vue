71d4b1a4cca84a59fc9851dc5fb7a63c7de52c0e
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-02-21 15:21:36 -0300

    implementacao do validation

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,16 +1,15 @@
 module.exports = {
-    client: 'postgresql',
-    connection: {
-      database: 'banco_vue',
-      user:     'postgres',
-      password: '123456'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-
+  client: "postgresql",
+  connection: {
+    database: "banco_vue",
+    user: "postgres",
+    password: "123456"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
+  }
 };
