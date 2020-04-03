a6b5e4d3eadaa3c281ab5e94eb129e2a36279587
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-04-02 21:22:59 -0300

    criando conexao com mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const app = require('express')();
 const consing = require('consign');
 const db = require('./config/db');
+const mongoose = require('mongoose')
+
+require('./config/mongodb')
 
 app.db = db;
+app.mongoose = mongoose
 
 consing()
   .include("./config/passport.js")
